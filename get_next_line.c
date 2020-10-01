b5534602bfa165dfc5136e76b3be3196823cb2a4
jricafor <jricafor@student.42.fr>
2020-10-01 09:21:23 +0000

    update

--- get_next_line.c
+++ get_next_line.c
@@ -12,18 +12,47 @@
 
 #include "get_next_line.h"
 
-int		get_next_line(int fd, char **line)
+char *set_line(char *buf, int start, int i)
 {
-	static int i = 0;
+	char *str;
+	int y;
 
-	if (i == 0)
-	{
-		// find next line
-	}
-	else
-	{
-		// find str
-		// then next line
-	}
-	// return 0, 1 or -1
+	str = malloc(sizeof(char) * (i - start) + 1);
+	y = 0;
+	while (start != i)
+		str[y++] = buf[start++];
+	str[y] = '\0';
+	return (str);
+}
+
+int	find_new_line(char *buf, char **line, int i)
+{
+	int start;
+
+	start = i;
+	while (buf[i] != '\n' && buf[i] != '\0')
+		i++;
+	*line = NULL;
+	*line = malloc(sizeof(char) * (i - start) + 1);
+	*line = set_line(buf, start, i);
+	if (buf[i + 1] && buf[i] == '\n')
+	 i++;
+	return (i);
+}
+
+int gnl(int fd, char **line)
+{
+	int		ret;
+	char	buf[BUFFER_SIZE + 1];
+	static	int	i = 0;
+
+	if (!line || fd < 0 || BUFFER_SIZE < 1 || read(fd, buf, 0) < 0)
+		return (-1);
+
+	ret = read(fd, buf, BUFFER_SIZE);
+	buf[ret] = '\0';
+	
+	i = find_new_line(buf, line, i);
+
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,7 +13,15 @@
 #ifndef	GET_NEXT_LINE_H
 #define	GET_NEXT_LINE_H
 
-#include <stdlib.h>
-int get_next_line(int fd, char **line);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+int		find_new_line(char *buf, char **line, int i);
+char	*set_line(char *buf, int start, int i);
 
 #endif
