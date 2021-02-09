93db77dff704a0041b2e54da7291e8eed582d7db
jricafor <jricafor@student.42.fr>
2021-02-09 17:02:27 +0000

    update

--- get_next_line.h
+++ get_next_line.h
@@ -21,20 +21,10 @@
 # include <unistd.h>
 # include <stdio.h>
 
+int		get_next_line(int fd, char **line);
 int		ft_strlen(const char *str);
 char	*join_line(char *tmp, char *sample, char *buff);
 char	*append_line(char *sample, char *buff, int size);
 int		read_buff(int fd, char *buff, char **line);
-int		set_line(char **line, int fd, char *buff, int i);
-int		get_next_line(int fd, char **line);
 
-<<<<<<< HEAD
-=======
-int		ft_strlen(char *str);
-int		looping(char *sample);
-char	*flegma(char *src, int start, int end);
-char	*append_sample(char *sample, char buf[BUFFER_SIZE + 1]);
-int		set_line(char *sample, char **line, char **tmp);
-
->>>>>>> 38c63828596e73ce071a79bd94a617ee13e09075
 #endif
